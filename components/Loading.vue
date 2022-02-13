67838351163533cc0dbf3981b7d8983f1c79ec6b
imanol <imanolcorimayo@gmail.com>
2022-02-12 22:23:39 -0300

    Loading component

---
+++ components/Loading.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container" }>
+    <img
+      src="http://www.gifde.com/gif/otros/decoracion/cargando-loading/cargando-loading-039.gif"
+      alt=""
+    />
+  </div>
+</template>
+
+<script>
+
+export default ({
+    name: 'Loading'
+})
+</script>
+
+<style scoped>
+    .container {
+        position: absolute;
+        top: 35vh;
+        right: 10vw;
+        border-radius: 50px;
+    }
+</style>
