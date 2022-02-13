a46b924cc87028b145aa999b9a2bed5844ec4fa0
imanol <imanolcorimayo@gmail.com>
2022-02-13 11:27:44 -0300

    add axios and get data from API

--- pages/Principal.vue
+++ pages/Principal.vue
@@ -2,17 +2,34 @@
   <div>
     <div>{ span }</div>
     
-    <div v-if="true"><Loading /></div>
+    <div v-if="loading"><Loading /></div>
     <div v-else><span>Holaa</span></div>
   </div>
 </template>
 
 <script>
 import Loading from "../components/Loading";
+import axios from 'axios'
 
 export default {
+  data() {
+    return {
+      loading: true,
+      pokemons: [],
+    }
+  },
   component: {
     Loading,
   },
+  async created() {
+    try {
+      const { data } = await axios.get('http://localhost:3001/pokemons');
+      this.pokemons = data;
+      this.loading = false;
+      console.log(data)
+    } catch (error) {
+      console.error(error)      
+    }
+  }
 };
 </script>
